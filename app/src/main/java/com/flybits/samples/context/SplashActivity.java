fe8c1a122960d756bc9f7cc2c3ee3f4632945e32
Petar Kramaric <petar.kramaric@gmail.com>
2016-04-28 11:33:44 -0400

    Added Check for Location which will activate the beacons and location context plugins.

--- app/src/main/java/com/flybits/samples/context/SplashActivity.java
+++ app/src/main/java/com/flybits/samples/context/SplashActivity.java
@@ -3,14 +3,20 @@ package com.flybits.samples.context;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.flybits.core.api.Flybits;
 import com.flybits.core.api.context.plugins.activity.ActivityProvider;
 import com.flybits.core.api.context.plugins.battery.BatteryLifeProvider;
+import com.flybits.core.api.context.plugins.beacon.BeaconProvider;
 import com.flybits.core.api.context.plugins.carrier.CarrierProvider;
 import com.flybits.core.api.context.plugins.language.LanguageProvider;
+import com.flybits.core.api.context.plugins.location.LocationProvider;
 import com.flybits.core.api.context.plugins.network.NetworkProvider;
 import com.flybits.core.api.exceptions.FeatureNotSupportedException;
 import com.flybits.core.api.interfaces.IRequestCallback;
@@ -21,16 +27,52 @@ import com.flybits.samples.context.utilities.ConnectivityUtils;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final int MY_PERMISSIONS_REQUEST_READ_LOCATION = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        checkForLoginPermissions();
+    }
 
+    private void checkForLoginPermissions() {
+
+        if (Build.VERSION.SDK_INT < 23) {
+            attemptToLogin(true);
+        } else {
+            if (ContextCompat.checkSelfPermission(SplashActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(SplashActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_READ_LOCATION);
+
+            } else if (ContextCompat.checkSelfPermission(SplashActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                attemptToLogin(true);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_LOCATION: {
+                boolean isLocationActivated = (grantResults.length > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED);
+                attemptToLogin(isLocationActivated);
+                return;
+            }
+        }
+    }
+
+    private void attemptToLogin(final boolean isLocationActivated) {
         if (ConnectivityUtils.isOnline(this)) {
             Flybits.include(SplashActivity.this).isUserLoggedIn(true, new IRequestLoggedIn() {
                 @Override
                 public void onLoggedIn(User user) {
-                    activateContext();
+                    activateContext(isLocationActivated);
 
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
@@ -39,24 +81,18 @@ public class SplashActivity extends AppCompatActivity {
 
                 @Override
                 public void onNotLoggedIn() {
-                    startLogin();
+                    startLogin(isLocationActivated);
                 }
             });
 
         } else {
-           showAlertDialog(R.string.errorTitleNoInternet, R.string.errorMessageNoInternet, R.string.errorBtnPositive);
+            showAlertDialog(R.string.errorTitleNoInternet, R.string.errorMessageNoInternet, R.string.errorBtnPositive);
         }
     }
 
-    private void startLogin() {
+    private void startLogin(final boolean isLocationActivated) {
 
         LoginOptions filterLogin;
-
-        /*
-            Check to see if the Remember Me Token has been saved. If so, use the remember me token
-            Otherwise, Prompt the user to Login.
-         */
-
         filterLogin = new LoginOptions.Builder(SplashActivity.this)
                 .loginAnonymously()
                 .setRememberMeToken()
@@ -67,7 +103,7 @@ public class SplashActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(User me) {
-                activateContext();
+                activateContext(isLocationActivated);
 
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 intent.putExtra("userID", me.id);
@@ -119,7 +155,7 @@ public class SplashActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-    private void activateContext(){
+    private void activateContext(final boolean isLocationActivated){
         try {
             ActivityProvider provider = new ActivityProvider(SplashActivity.this, 60000);
             Flybits.include(SplashActivity.this).activateContext(null, provider);
@@ -135,6 +171,14 @@ public class SplashActivity extends AppCompatActivity {
 
             NetworkProvider provider5 = new NetworkProvider(SplashActivity.this, 60000);
             Flybits.include(SplashActivity.this).activateContext(null, provider5);
+
+            if (isLocationActivated){
+                LocationProvider provider6 = new LocationProvider(SplashActivity.this, 60000);
+                Flybits.include(SplashActivity.this).activateContext(null, provider6);
+
+                BeaconProvider provider7 = new BeaconProvider(SplashActivity.this, 60000);
+                Flybits.include(SplashActivity.this).activateContext(null, provider7);
+            }
         }catch (FeatureNotSupportedException exception){
 
         }
